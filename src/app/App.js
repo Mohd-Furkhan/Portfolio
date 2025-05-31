7a0b61a0d9d532513de95690103a8b5a9047e8a0
Mohammed Furkhan <mfurkhan955@gmail.com>
2025-06-01 02:33:57 +0530

    First commit

---
+++ src/app/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/Navbar/Component/Navbar.jsx";
+import Home from "./pages/Home/Component/Home.jsx";
+import About from "./pages/About/Component/About.jsx";
+import Projects from "./pages/Projects/Component/Projects.jsx";
+import Skills from "./pages/Skills/Component/Skills.jsx";
+import Contact from "./pages/Contact/Component/Contact.jsx";
+import Resume from "./pages/Resume/Component/Resume.jsx";
+import Footer from "./pages/Footer/Component/Footer.jsx";
+
+function App() {
+  return (
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/Footer" element={<Footer />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/app/pages/Footer/Component/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import "./Footer.css"
+
+function Footer()
+{
+    return(
+        <>
+            <footer>
+                <ul class="nav justify-content-center border-bottom pb-3 mb-2 mt-2">
+                    <li className="nav-item footer-btn">
+                    <Link className="nav-link text-light" to="/">
+                        Home
+                    </Link>
+                    </li>
+                    <li class="nav-item footer-btn">
+                    <Link className="nav-link text-light" to="/about">
+                        About
+                    </Link>
+                    </li>
+                    <li class="nav-item footer-btn">
+                    <Link className="nav-link text-light" to="/projects">
+                        Projects
+                    </Link>
+                    </li>
+                    <li class="nav-item footer-btn">
+                    <Link className="nav-link text-light" to="/skills">
+                        Skills
+                    </Link>
+                    </li>
+                    <li class="nav-item footer-btn">
+                    <Link className="nav-link text-light" to="/contact">
+                        Contact
+                    </Link>
+                    </li>
+                </ul>
+                <p class="text-center text-light">
+                    © Copyright 2025 All rights reserved. <br /> 
+                    <span>Designed by &lt; Mohammed Furkhan /&gt;</span>
+                </p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
